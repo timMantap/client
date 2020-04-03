8716574b5aa92096e05995234f2c6d4c6d6f779e
hariszaki17 <hariszaki17@gmail.com>
2020-04-03 08:00:37 +0700

    ajax client

--- main.js
+++ main.js
@@ -1,46 +1,64 @@
+const baseUrl = `http://localhost:8080`
+
 $( document ).ready(function() {
     auth()
 })
 
-const hideElement = element => {
-    return $(element).hide()
+// const hideElement = element => {
+//     return $(element).hide()
+// }
+
+// const showElement = element => {
+//     return $(element).show()
+// }
+
+// const hideShowElement = (target, hide, show) => {
+//     $(target).on('click', (e) => {
+//         e.preventDefault()
+//         hideElement(hide)
+//         showElement(show)
+//     })
+// }
+
+const toSignup = (e) => {
+    e.preventDefault()
+    $( '#nav-container' ).hide()
+    $( '#page-content-wrapper-food' ).hide()
+    $( '#signin-container' ).hide()
+    $( '#signup-container' ).show()
 }
 
-const showElement = element => {
-    return $(element).show()
-}
-
-const hideShowElement = (target, hide, show) => {
-    $(target).on('click', (e) => {
-        e.preventDefault()
-        hideElement(hide)
-        showElement(show)
-    })
+const toSignin = (e) => {
+    e.preventDefault()
+    $( '#nav-container' ).hide()
+    $( '#page-content-wrapper-food' ).hide()
+    $( '#signup-container' ).hide()
+    $( '#signin-container' ).show()
 }
 
 function auth() {
-    if (localStorage.getItem('token')) {
-        hideElement('#signup-container')
-        hideElement('#signin-container')
+    if (localStorage.token) {
+        $( '#signup-container' ).hide()
+        $( '#signin-container' ).hide()
         $( '#page-content-wrapper-food' ).show()
-        $( '#navbar' ).show()
+        $( '#nav-container' ).show()
+        readMain()
     } else {
+        $( '#signin-container' ).show()
+        $( '#nav-container' ).hide()
         $( '#page-content-wrapper-food' ).hide()
-        $( '#navbar' ).hide()
-        showElement('#signup-container')
-        hideElement('#signin-container')
-        hideShowElement('#signup-bottom-btn','#signin-container','#signup-container')
-        hideShowElement('#signin-bottom-btn','#signup-container','#signin-container')
+        $( '#signup-container' ).hide()
     }
 }
 
-function signOut() {
+function signOut(e) {
+    e.preventDefault()
     localStorage.clear()
+    auth()
     let auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
     });
-    auth()
 }
 
 $('#signup-btn').click(e => {
@@ -78,11 +96,16 @@ $('#signin-btn').click(e => {
     })
     .done(result => {
         localStorage.setItem('token', result.token)
+        $('#email-signin').val('')
+        $('#password-signin').val('')
         auth()
     })
     .fail(err => {
         console.log(err)
         formnotif(err.responseJSON.errors[0].message)
+        $('#email-signin').val('')
+        $('#password-signin').val('')
+        auth()
     })
 })
 
@@ -102,6 +125,42 @@ function onSignIn(googleUser) {
     .fail(err => {
         console.log(err)
         formnotif(err.responseJSON.errors[0].message)
+    })
+}
+
+const readMain = () => {
+    $( '#card-container' ).empty()
+    $.ajax({
+        method: 'GET',
+        headers: {
+            'token': localStorage.token
+        },
+        url: `http://localhost:3000/feature`
+    })
+    .done(data => {
+        // console.log(data)
+        let appendItems = ''
+        data.restaurants.forEach(el => {
+            console.log(el)
+            appendItems += `<div class="card col-xl-3 m-3 d-flex flex-column align-items-center" id="card" style="width:200px">
+              <div class="card-body">
+                <img src="${el.thumb}" class="img-fluid" alt="${el.name}">
+                  <h4 class="card-title">${el.name}</h4>
+                  <p class="card-text">Cuisine: ${el.cuisines}</p>
+                  <p class="card-text">Average cost: ${el.avg_cost_for_two}</p>
+                  <p class="card-text">Open: ${el.timings}</p>
+                  <p class="card-text">${el.address}</a><br><br>
+                  <a href="${el.url}" class="btn btn-primary">Visit Place</a>
+                </div>
+            </div>\n`
+        })
+        $( '#card-container' ).append(appendItems)
+    })
+    .fail(err => {
+        console.log(err)
+        // err.responseJSON.errors.forEach(data => {
+        //     console.log(data.message)
+        // })
     })
 }
 
